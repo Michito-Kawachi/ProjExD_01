cd7b7f0bf817ad486eeeae874fab1880405669e1
michito_K <c0b2204560@edu.teu.ac.jp>
2023-09-26 15:38:23 +0900

    問6

--- pygame_image.py
+++ pygame_image.py
@@ -12,14 +12,17 @@ def main():
     kt3_img = pg.image.load("ex01/fig/3.png")
     kt3_img = pg.transform.flip(kt3_img, True, False)
     #問3
-    kt3_img_rotate = pg.transform.rotate(kt3_img, 10)
+    kt3_img_rotate = pg.transform.rotozoom(kt3_img, 10, 1.0)
     img_lst = [kt3_img, kt3_img_rotate]
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        #問6 
+        screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg_img, [1600-tmr, 0])
+        if tmr >= 1599:
+            tmr = 0
         #問5
         if tmr % 10 > 5:
             screen.blit(img_lst[0], [300, 200])
@@ -27,7 +30,7 @@ def main():
             screen.blit(img_lst[1], [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
