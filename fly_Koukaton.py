566849ce2bcd017c69d0503b1e53dd4b4a1dff43
michito_K <c0b2204560@edu.teu.ac.jp>
2023-09-26 14:42:55 +0900

    問1

---
+++ fly_Koukaton.py
@@ -0,0 +1,13 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_mode((1600, 900))
+    back_img = pg.image.oad("./fig/pg_bg.jpg")
+    
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
