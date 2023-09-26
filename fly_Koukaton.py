3b282e0b793813aa86459f1893e4f69af12cdb76
michito_K <c0b2204560@edu.teu.ac.jp>
2023-09-26 14:56:23 +0900

    問3

--- fly_Koukaton.py
+++ fly_Koukaton.py
@@ -3,9 +3,14 @@ import sys
 
 def main():
     pg.display.set_mode((1600, 900))
+    #問1
     back_img = pg.image.load("./fig/pg_bg.jpg")
+    #問2
     kt3_img = pg.image.load("./fig/3.png")
     kt3_img = pg.transform.flip(kt3_img, True, False)
+    #問3
+    kt3_img_rotate = pg.transform.rotate(kt3_img, 10)
+    img_lst = [kt3_img, kt3_img_rotate]
 
 if __name__ == "__main__":
     pg.init()
